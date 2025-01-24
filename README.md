033f46269a195be878dbffb83ce054167a9ac634
SrBaliardo <tao.john007@icloud.com>
2025-01-24 10:59:33 -0300

    fix(code): update README.

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # JHAB-Play is a personal music player for listening and relaxing. You can put your favorite songs on it.
 
+- Você pode ouvir em ordem ou pressionar o botão para ouvir em ordem aleatória.
+- Você pode pressionar o botão para repetir seus favoritos.
+- Você pode pular para a melhor parte da música na barra de progresso.
+- Além disso, você pode dar um “coração” àquelas que tocam seu coração!
+
+Para executar o código, você precisa ter o VisualCode em seu computador e clonar o repositório.
+
+Um dos primeiros projetos que desenvolvi para prática de HTML-CSS-JavaScript na aplicação em "music-players".
+
+Este é bem simples e não faz consumo de API, tem musicas pré-carregadas localmente.
+
+"Não pague e nem perca sua playlist XD"
+
+<br>
+
+# JHAB-Play is a personal music player for listening and relaxing. You can put your favorite songs on it.
+
 - You can listen in order or press the button to listen in random order.
 - You can press the button to repeat your favorites.
 - You can skip to the best part of the song in the progress bar.
@@ -7,11 +24,17 @@
 
 To run the code, you need to have VisualCode on your computer and then clone the repository.
 
+One of the first projects I developed to practise HTML-CSS-JavaScript in ‘music-players’.
+
+This one is very simple and doesn't use the API, it has songs pre-loaded locally.
+
+‘Don't pay or lose your playlist XD’
+
 You can view the project via the link: <br>
 https://srbaliardo.github.io/music-player
 
 <br>
 
-" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> | 
-  <img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> | 
-  <img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
+" <img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"> |
+<img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"> |
+<img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"> "
--- script.js
+++ script.js
@@ -13,219 +13,228 @@ const songTime = document.getElementById("song-time");
 const totalTime = document.getElementById("total-time");
 const likeButton = document.getElementById("like");
 
-
 //music var //////////////////////////////////////////////
 const music1 = {
-    songName: "Redbone",
-    artist: "Childish Gambino",
-    file: "childish-gambino-redbone",
-    liked: false,
+  songName: "Redbone",
+  artist: "Childish Gambino",
+  file: "childish-gambino-redbone",
+  liked: false,
 };
 
 const music2 = {
-    songName: "Believer",
-    artist: "Imagine Dragons",
-    file: "imagine-dragons-believer",
-    liked: false,
+  songName: "Believer",
+  artist: "Imagine Dragons",
+  file: "imagine-dragons-believer",
+  liked: false,
 };
 
 const music3 = {
-    songName: "Enemy",
-    artist: "Imagine Dragons feat. JID",
-    file: "imagine-dragons-jid-enemy",
-    liked: false,
+  songName: "Enemy",
+  artist: "Imagine Dragons feat. JID",
+  file: "imagine-dragons-jid-enemy",
+  liked: false,
 };
 
 const music4 = {
-    songName: "Wait",
-    artist: "M83",
-    file: "m83-wait",
-    liked: false,
+  songName: "Wait",
+  artist: "M83",
+  file: "m83-wait",
+  liked: false,
 };
 
 const music5 = {
-    songName: "Take Me Somewhere Nice",
-    artist: "Mogwai",
-    file: "mogwai-take-me-somewhere-nice",
-    liked: false,
+  songName: "Take Me Somewhere Nice",
+  artist: "Mogwai",
+  file: "mogwai-take-me-somewhere-nice",
+  liked: false,
 };
 
 const music6 = {
-    songName: "Snuff",
-    artist: "Slipknot",
-    file: "slipknot-snuff",
-    liked: false,
+  songName: "Snuff",
+  artist: "Slipknot",
+  file: "slipknot-snuff",
+  liked: false,
 };
 
 const music7 = {
-    songName: "Stubbornd Love",
-    artist: "The Lumineers",
-    file: "the-lumineers-stubborn-love",
-    liked: false,
+  songName: "Stubbornd Love",
+  artist: "The Lumineers",
+  file: "the-lumineers-stubborn-love",
+  liked: false,
 };
 
 const music8 = {
-    songName: "Chlorine",
-    artist: "Twenty One Pilots",
-    file: "twenty-one-pilots-chlorine",
-    liked: false,
+  songName: "Chlorine",
+  artist: "Twenty One Pilots",
+  file: "twenty-one-pilots-chlorine",
+  liked: false,
 };
 
 const music9 = {
-    songName: "Youth",
-    artist: "Daughter",
-    file: "youth-daughter",
-    liked: false,
+  songName: "Youth",
+  artist: "Daughter",
+  file: "youth-daughter",
+  liked: false,
 };
 
 ///////////////////////////////////////////////////////////////////////
 
 let isPlaying = false;
-const originalPlaylist = JSON.parse(localStorage.getItem("playlist")) ?? [music1, music2, music3,music4, music5, music6, music7, music8, music9];
+const originalPlaylist = JSON.parse(localStorage.getItem("playlist")) ?? [
+  music1,
+  music2,
+  music3,
+  music4,
+  music5,
+  music6,
+  music7,
+  music8,
+  music9,
+];
 let index = 3;
 let isShuffled = false;
 let sortedPlaylist = [...originalPlaylist];
 let repeatOn = false;
 
-
-function playSong(){
-    song.play();
-    playPause.querySelector(".bi").classList.remove("bi-play-circle-fill");
-    playPause.querySelector(".bi").classList.add("bi-pause-circle-fill");
+function playSong() {
+  song.play();
+  playPause.querySelector(".bi").classList.remove("bi-play-circle-fill");
+  playPause.querySelector(".bi").classList.add("bi-pause-circle-fill");
 }
 
-function pauseSong(){
-    song.pause();
-    playPause.querySelector(".bi").classList.remove("bi-pause-circle-fill");
-    playPause.querySelector(".bi").classList.add("bi-play-circle-fill");
+function pauseSong() {
+  song.pause();
+  playPause.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+  playPause.querySelector(".bi").classList.add("bi-play-circle-fill");
 }
 
-function playPauseDecider(){
-    if (isPlaying === false){
-        playSong();
-        isPlaying = true
-    }else {
-        pauseSong();
-        isPlaying = false
-    }
-}
-
-function initializeSong(){
-    cover.src = `./images/${sortedPlaylist[index].file}.jpg`;
-    song.src = `./songs/${sortedPlaylist[index].file}.mp3`; 
-    songName.innerText = sortedPlaylist[index].songName;
-    bandName.innerText = sortedPlaylist[index].artist;
-    likeButtonRender();
-}
-
-function previousSong(){
-    if(index === 0){
-        index = sortedPlaylist.length - 1;
-    }else {
-        index -=1;
-    }
-    initializeSong();
+function playPauseDecider() {
+  if (isPlaying === false) {
     playSong();
+    isPlaying = true;
+  } else {
+    pauseSong();
+    isPlaying = false;
+  }
 }
 
-function nextSong(){
-    if(index === sortedPlaylist.length - 1){
-        index = 0;
-    }else {
-        index +=1;
-    }
-    initializeSong();
+function initializeSong() {
+  cover.src = `./images/${sortedPlaylist[index].file}.jpg`;
+  song.src = `./songs/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender();
+}
+
+function previousSong() {
+  if (index === 0) {
+    index = sortedPlaylist.length - 1;
+  } else {
+    index -= 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function nextSong() {
+  if (index === sortedPlaylist.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function updateProgress() {
+  const barWidth = (song.currentTime / song.duration) * 100;
+  currentProgress.style.setProperty("--progress", `${barWidth}%`);
+  songTime.innerText = toHHMMSS(song.currentTime);
+}
+
+function jumpTo(event) {
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition / width) * song.duration;
+  song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while (currentIndex > 0) {
+    let randomIndex = Math.floor(Math.random() * size);
+    let aux = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+    preShuffleArray[randomIndex] = aux;
+    currentIndex -= 1;
+  }
+}
+
+function shuffleButtonClicked() {
+  if (isShuffled === false) {
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add("button-active");
+  } else {
+    isShuffled = false;
+    shuffleArray(...originalPlaylist);
+    shuffleButton.classList.remove("button-active");
+  }
+}
+
+function repeatButtonClicked() {
+  if (repeatOn === false) {
+    repeatOn = true;
+    repeatButton.classList.add("button-active");
+  } else {
+    repeatOn = false;
+    repeatButton.classList.remove("button-active");
+  }
+}
+
+function nextOrRepeat() {
+  if (repeatOn === false) {
+    nextSong();
+  } else {
     playSong();
+  }
 }
 
-function updateProgress(){
-    const barWidth = (song.currentTime/song.duration)*100;
-    currentProgress.style.setProperty("--progress",`${barWidth}%`);songTime.innerText = toHHMMSS(song.currentTime);
+function toHHMMSS(originalNumber) {
+  let hours = Math.floor(originalNumber / 3600);
+  let min = Math.floor((originalNumber - hours * 3600) / 60);
+  let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+  return `${hours.toString().padStart(2, "0")}:${min
+    .toString()
+    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 }
 
-function jumpTo(event){
-    const width = progressContainer.clientWidth;
-    const clickPosition = event.offsetX;
-    const jumpToTime = (clickPosition/width)* song.duration;
-    song.currentTime = jumpToTime;
+function updateTotalTime() {
+  totalTime.innerText = toHHMMSS(song.duration);
 }
 
-  function shuffleArray(preShuffleArray){
-    const size = preShuffleArray.length;
-    let currentIndex = size - 1;
-    while(currentIndex > 0){
-        let randomIndex = Math.floor(Math.random()* size);
-        let aux = preShuffleArray[currentIndex];
-        preShuffleArray[currentIndex] = preShuffleArray[randomIndex]
-        preShuffleArray[randomIndex] = aux;
-        currentIndex -= 1;
-    }
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked === true) {
+    likeButton.querySelector(".bi").classList.remove("bi-heart");
+    likeButton.querySelector(".bi").classList.add("bi-heart-fill");
+    likeButton.classList.add("button-active");
+  } else {
+    likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
+    likeButton.querySelector(".bi").classList.add("bi-heart");
+    likeButton.classList.remove("button-active");
+  }
 }
 
-  function shuffleButtonClicked(){
-    if(isShuffled === false){
-        isShuffled = true;
-        shuffleArray(sortedPlaylist);
-        shuffleButton.classList.add("button-active");
-    }else {
-        isShuffled = false;
-        shuffleArray(...originalPlaylist);
-        shuffleButton.classList.remove("button-active");
-    }
-}
-
-function repeatButtonClicked(){
-    if(repeatOn === false){
-        repeatOn = true;
-        repeatButton.classList.add("button-active");
-    }else {
-        repeatOn = false;
-        repeatButton.classList.remove("button-active");
-    }
-}
-
-function nextOrRepeat(){
-    if(repeatOn === false){
-        nextSong();
-    }else {
-        playSong();
-    }
-}
-
-function toHHMMSS(originalNumber){
-    let hours = Math.floor(originalNumber/3600);
-    let min = Math.floor((originalNumber - hours * 3600)/60);
-    let secs = Math.floor(originalNumber - hours * 3600 - min*60);
-
-    return `${hours.toString().padStart(2, '0')}:${min.toString()
-      .padStart(2, '0')}:${secs.toString().padStart(2, "0")}`;
-}
-  
-function updateTotalTime(){
-    totalTime.innerText = toHHMMSS(song.duration);
-}
-
-function likeButtonRender(){
-    if (sortedPlaylist[index].liked === true){
-        likeButton.querySelector(".bi").classList.remove("bi-heart");
-        likeButton.querySelector(".bi").classList.add("bi-heart-fill");
-        likeButton.classList.add("button-active");
-    }else {
-        likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
-        likeButton.querySelector(".bi").classList.add("bi-heart");
-        likeButton.classList.remove("button-active");
-    }
-}
-
-function likeButtonClicked(){
-    if (sortedPlaylist[index].liked === false) {
-      sortedPlaylist[index].liked = true;
-    } else {
-      sortedPlaylist[index].liked = false;
-    }
-    likeButtonRender();
-    localStorage.setItem("playlist", JSON.stringify(originalPlaylist));
-
+function likeButtonClicked() {
+  if (sortedPlaylist[index].liked === false) {
+    sortedPlaylist[index].liked = true;
+  } else {
+    sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+  localStorage.setItem("playlist", JSON.stringify(originalPlaylist));
 }
 
 initializeSong();
